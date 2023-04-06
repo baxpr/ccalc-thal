049970e00482ec61ca48e86c74d8841496de99f3
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-04-06 08:37:43 -0500

    split hemispheres

--- rois/thalamus-mask/create-thalamus-mask.sh
+++ rois/thalamus-mask/create-thalamus-mask.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #
-# Thalamus mask created by binarizing, then dilating Yeo thalamus
+# Yeo thalamus ROIs are in FSL's MNI152 space
 
+# Thalamus mask created by binarizing, then dilating Yeo thalamus
 fslmaths ../Yeo-thalamus/1000subjects_TightThalamus_clusters007_ref \
     -bin -dilF \
     thalamus-mask
