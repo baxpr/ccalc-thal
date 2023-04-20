9534dcd1fa9db026bf90927b1008149f13254878
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-04-20 14:42:42 -0500

    info

--- matlab/src/entrypoint.m
+++ matlab/src/entrypoint.m
@@ -16,9 +16,6 @@ inp = P.Results;
 warning('off','MATLAB:table:ModifiedAndSavedVarnames');
 
 
-%% Compute PCs
-
-
 %% Get Yeo7 network names for Schaefer400 ROI set
 %
 % Yeo7 ROIs with labels from Schaefer CSV:
@@ -39,34 +36,8 @@ for h = 1:height(netmap)
 end
 
 
-%% Maps for Schaefer 400 in thalamus mask
-
-% FIXME We are getting nan PC values. Voxels with no connections will get
-% PC of nan.
-
-% Get voxel time series to csv then use same func as for time series csvs
-disp('Computing PC on image')
-voxels_csv = fullfile(inp.out_dir,'thalamusvoxels.csv');
-voxels_to_csv(inp.wfmri_nii,inp.mask_nii,voxels_csv);
-compute_PCs( ...
-    inp, ...
-    inp.schaefer_csv, ...
-    voxels_csv, ...
-    netmap.Network, ...
-    'schaefer400_voxels' ...
-    );
-
-% Remap to mean PC image inside thalamus mask
-meanPC_to_image( ...
-    inp.mask_nii, ...
-    fullfile(inp.out_dir,'meanPC_schaefer400_voxels.csv'), ...
-    fullfile(inp.out_dir,'meanPC_schaefer400_voxels.nii') ...
-    );
-
-
-
-%% Compute PCs against Schaefer 400 set
-disp('Computing PC on matrices')
+%% Compute PCs against Schaefer 400 ROIs. Also stores conn matrices
+disp('Computing PC on time series')
 compute_PCs( ...
     inp, ...
     inp.schaefer_csv, ...
@@ -90,20 +61,33 @@ compute_PCs( ...
     );
 
 
-return
+%% Voxelwise PC against Schaefer 400 ROIs, within thalamus mask
+% Voxels with no connections will get PC of nan.
+
+% Get voxel time series to csv then use same func as for time series csvs
+disp('Computing PC on image')
+voxels_csv = fullfile(inp.out_dir,'thalamusvoxels.csv');
+voxels_to_csv(inp.wfmri_nii,inp.mask_nii,voxels_csv);
+compute_PCs( ...
+    inp, ...
+    inp.schaefer_csv, ...
+    voxels_csv, ...
+    netmap.Network, ...
+    'schaefer400_voxels' ...
+    );
+
+% Remap to mean PC image inside thalamus mask
+meanPC_to_image( ...
+    inp.mask_nii, ...
+    fullfile(inp.out_dir,'meanPC_schaefer400_voxels.csv'), ...
+    fullfile(inp.out_dir,'meanPC_schaefer400_voxels.nii') ...
+    );
 
 
-%% Maps, Schaefer 400. Only within mask of dilated Yeo7 whole thalamus
+%% Conn maps for Schaefer 400 ROIs within mask of dilated Yeo7 whole thalamus
 disp('Connectivity maps')
-R4 = compute_connmaps(inp.schaefer_csv,inp.wfmri_nii,inp.mask_nii, ...
+compute_connmaps(inp.schaefer_csv,inp.wfmri_nii,inp.mask_nii, ...
     fullfile(inp.out_dir,'connmaps_schaefer400'));
 
-% Participation coefficient. Compute for Thomas/Yeo/voxel thalamus
-% segmentations and Schaefer cortical segmentation. Save to csv for the
-% first two. For thalamus voxel we need to save to image though. Would be
-% good to save them all to image, but we'd need to map the Thomas ROIs into
-% MNI space first to make that work (and map ROI image labels to matrix
-% labels correctly). General solution there is to create MNI space ROI
-% images in the wfmri space for all ROI sets, with a corresponding
-% index-to-label mapping csv that matches the matrix csvs we are putting
-% out.
+
+
