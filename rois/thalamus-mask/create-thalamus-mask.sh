4cf4902bb8885346b68a048593769b111bea9ba0
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-04-21 09:39:25 -0500

    remask

--- rois/thalamus-mask/create-thalamus-mask.sh
+++ rois/thalamus-mask/create-thalamus-mask.sh
@@ -2,7 +2,8 @@
 #
 # Yeo thalamus ROIs are in FSL's MNI152 space
 
-# Thalamus mask created by binarizing, then dilating Yeo thalamus
+# Thalamus mask created by binarizing Yeo thalamus
+# Could add -dilF to dilate
 fslmaths ../Yeo-thalamus/1000subjects_TightThalamus_clusters007_ref \
-    -bin -dilF \
+    -bin \
     thalamus-mask
