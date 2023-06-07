a37729e03a765cb7277204e3d5691a3021c2636c
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-06-07 14:15:59 -0500

    labels

--- matlab/src/compute_PCs.m
+++ matlab/src/compute_PCs.m
@@ -1,20 +1,26 @@
-function compute_PCs(inp,roi_csv1,roi_csv2,row_networks,tag)
+function compute_PCs(inp,roi1_csv,roi2_csv,roi1_labels,tag)
 
 % Compute connectivity matrix
-[R,~,colnames] = compute_connmat( ...
-    roi_csv1, ...
-    roi_csv2, ...
+[R,rownames,colnames] = compute_connmat( ...
+    roi1_csv, ...
+    roi2_csv, ...
     inp.connmetric, ...
     inp.out_dir, ...
     tag);
 
+% Verify labels and data match for roi1
+if ~all(strcmp(rownames',roi1_labels.Region))
+    error('Mismatch in region labels')
+end
+
 % If the two ROI files are the same, zero the diagonal to prevent
 % self-connections interfering with PC calculation
 Rmat = table2array(R);
-if strcmp(roi_csv1,roi_csv2)
+if strcmp(roi1_csv,roi2_csv)
+    disp('Zeroing connmat diagonal for matching ROI sets')
     Rmat = Rmat - eye(size(Rmat)) .* Rmat;
 end
-    
+
 % PC
 densities = eval(inp.densities);
 PCs = [];
@@ -27,7 +33,7 @@ for d = densities
     % Or just binarize
     %thisR = double(thisR >= quantile(thisR(:),1-d));
     
-    PCs = [PCs; participation_coeff(thisR,row_networks)];
+    PCs = [PCs; participation_coeff(thisR,roi1_labels.Network)];
 end
 
 % Organize and write to file
