8af628084f73e0f2250094a2109c8754ea71d98b
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-06-21 17:56:59 -0500

    refactor?

---
+++ matlab/src/testing_bct.m
@@ -0,0 +1,13 @@
+% Testing use of BCT for incomplete matrix analysis
+
+% We will need Yeo7 network labels for Yeo thalamus ROIs and thalamus voxels
+
+% Add a single thalamus ROI or voxel time series to the Schaefer time
+% series data. Combine the network labels info to match.
+
+% Compute the conn matrix and threshold, zero diag, etc. Compute PC and WMD
+% for the matrix using BCT functions. Extract the specific value for the
+% thalamus ROI.
+
+% Repeat over thresholds and average. Repeat over thalamus ROIs.
+
