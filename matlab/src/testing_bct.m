0458139e0cc8dcfe13a2b6d6dff8f6d0f304c61e
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-06-27 11:32:11 -0400

    schaefer network nums

--- matlab/src/testing_bct.m
+++ matlab/src/testing_bct.m
@@ -11,3 +11,35 @@
 
 % Repeat over thresholds and average. Repeat over thalamus ROIs.
 
+% Inputs:
+%    Schaefer cortical ROI time series with network labels
+%    Single thalamus ROI or voxel time series with network label
+
+
+
+% Metrics:
+%    Degree centrality
+%    Participation coefficient
+%    Within-module degree
+%    Modularity
+
+
+
+%% Steps:
+
+% Create time series data matrix with Yeo7 network labels and network label
+% number
+
+% Compute connectivity matrix with specific metric e.g. bivariate
+% correlation
+
+% Threshold and binarize the matrix as desired
+
+% Compute DC, PC, WMD
+
+
+
+
+%%
+
+% Load schafer data
--- rois/Schaefer2018/make_labels_csv.py
+++ rois/Schaefer2018/make_labels_csv.py
@@ -11,7 +11,16 @@ networks = pandas.read_csv(networks_csv)
 networks['Label'] = networks['ROI Label']
 networks['Region'] = [f'schaefer_{x:03d}' for x in networks['ROI Label']]
 networks['Network'] = [x.split('_')[2] for x in networks['ROI Name']]
+networks['NetworkNum'] = [0 for x in networks['Network']]
 
-networks = networks[['Label','Region','Network']]
+networks.loc[networks.Network=='Vis', 'NetworkNum'] = 1
+networks.loc[networks.Network=='SomMot', 'NetworkNum'] = 2
+networks.loc[networks.Network=='DorsAttn', 'NetworkNum'] = 3
+networks.loc[networks.Network=='SalVentAttn', 'NetworkNum'] = 4
+networks.loc[networks.Network=='Limbic', 'NetworkNum'] = 5
+networks.loc[networks.Network=='Cont', 'NetworkNum'] = 6
+networks.loc[networks.Network=='Default', 'NetworkNum'] = 7
+
+networks = networks[['Label','Region','Network','NetworkNum']]
 
 networks.to_csv(f'Schaefer2018_400Parcels_7Networks_order_FSLMNI152_2mm-labels.csv', index=False)
