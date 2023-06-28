ef9870bb5ff3c52fa944af202cd20ce2a0ac15ff
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-06-28 09:21:13 -0400

    working on BCT refactor

--- matlab/src/testing_bct.m
+++ matlab/src/testing_bct.m
@@ -37,7 +37,45 @@
 
 % Compute DC, PC, WMD
 
+% Load Schaefer data and network labels
+data_schaefer = readtable('../../OUTPUTS/schaefer.csv');
+info_schaefer = readtable('../../OUTPUTS/schaefer-networks.csv');
 
+% Get actual varnames from the data, and re-order network names and labels
+% to match
+info_schaefer = outerjoin( ...
+    table(data_schaefer.Properties.VariableNames','VariableNames',{'Region'}), ...
+    info_schaefer, ...
+    'Keys',{'Region'}, ...
+    'MergeKeys',true, ...
+    'Type','left' ...
+    );
+communities_schaefer = info_schaefer.NetworkNum;
+
+% Same for Yeo ROIs
+data_yeo = readtable('../../OUTPUTS/yeo.csv');
+info_yeo = readtable('../../OUTPUTS/yeo-networks.csv');
+info_yeo = outerjoin( ...
+    table(data_yeo.Properties.VariableNames','VariableNames',{'Region'}), ...
+    info_yeo, ...
+    'Keys',{'Region'}, ...
+    'MergeKeys',true, ...
+    'Type','left' ...
+    );
+communities_yeo = info_yeo.NetworkNum;
+
+
+% Combine data for a single Yeo thalamus ROI
+data_roi = data_yeo;
+communities_roi = communities_yeo;
+this_index = 1;
+data_this = [data_schaefer data_roi(:,this_index)];
+communities_this = [communities_schaefer; communities_roi(this_index)];
+
+
+% Correlation matrix with diagonals set to zero
+R = corr(table2array(data_this));
+R = R - R.*eye(size(R));
 
 % As a function of threshold 0..1:
 
@@ -53,17 +91,38 @@
 %  WMD       module_degree_zscore
 
 
+% We will assume our ROI of interest is the final entry in the matrix
+thresholds = 0:0.02:1;
 
-for threshold = 0:0.02:1
+result = table();
+for t = 1:numel(thresholds)
 
+    result.threshold(t,1) = thresholds(t);
+    
+    % Threshold only
+    %Cw = R;
+    %Cw(R<threshold) = 0;
+    
+    % Threshold and binarize
+    C = double(R>=thresholds(t));
+    
+    [~,comp_sizes] = get_components(C);
+    result.ncomponents(t,1) = numel(comp_sizes);
+    result.density(t,1) = density_und(C);
+    degree = degrees_und(C);
+    result.degree(t,1) = degree(end);
+    
     % For PC, we are only capturing the value for the thalamus node, and
     % the thalamus node's community assignment is irrelevant to its
     % computation so we just choose 1 if it's not already specified.
+    PC = participation_coef(C,communities_this);
+    result.PC(t,1) = PC(end);
     
     % For WMD, the thalamus node must be assigned to a particular
     % community, so if it's not specified we skip this metric (e.g. for
     % THOMAS ROIs).
-    
+    WMD = module_degree_zscore(C,communities_this);
+    result.WMD(t,1) = WMD(end);
     
 end
 
