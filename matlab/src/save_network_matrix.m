8bf5de2967e03059d65ae3f7b87f0d4f7af53a8b
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-08-25 13:24:39 -0500

    reorg R mat file

---
+++ matlab/src/save_network_matrix.m
@@ -0,0 +1,22 @@
+function R_csv = save_network_matrix(data,threshold,roi_set,out_dir)
+
+% Prep connectivity matrix for BCT network computations
+
+% Correlation matrix with diagonals set to zero
+R = corr(table2array(data));
+R = R - R.*eye(size(R));
+
+% Threshold only
+C = R;
+C(R<threshold) = 0;
+
+% Additionally, binarize
+%C = double(R>=threshold);
+
+% Get variable names
+Rvarnames = data.Properties.VariableNames;
+
+% Write table
+saveR = array2table(R,'VariableNames',Rvarnames,'RowNames',Rvarnames);
+R_csv = fullfile(out_dir,['R_' roi_set '.csv']);
+writetable(saveR,R_csv,'WriteRowNames',true);
