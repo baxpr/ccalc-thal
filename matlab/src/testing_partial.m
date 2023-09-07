a16f273101d616341cbac81a68c0ae5a43788a19
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-09-07 10:49:54 -0500

    reorg again for partial corr

---
+++ matlab/src/testing_partial.m
@@ -0,0 +1,19 @@
+% Compute metrics from
+%    schaefer-schaefer pearson correlation matrix
+%      and
+%    schaefer-thalamus partial correlation matrix
+
+out_dir = '../../OUTPUTS';
+
+% Get the time series data previously created by roi_extract.sh
+[schaefer,yeo,voxel,thomas] = get_time_series(out_dir);
+
+% Compute Pearson R for Schaefer x Schaefer (cortical) ROIs and save
+
+% Compute partial R for Schaefer x Thalamus for each thalamus set and save
+
+
+% Compute PC at each density threshold for the Schaefer x Thalamus matrices
+
+
+% Compute WMD
