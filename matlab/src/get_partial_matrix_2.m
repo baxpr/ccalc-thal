b43024a02a9d15a11242e28e8af0bb52390832c5
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-09-07 12:34:10 -0500

    partials

--- matlab/src/get_partial_matrix_2.m
+++ matlab/src/get_partial_matrix_2.m
@@ -5,25 +5,22 @@ warning('off','stats:pca:ColRankDefX');
 Rp = nan(width(thal),width(schaefer));
 
 for s = 1:width(schaefer)
-    for t = 1:width(thal)
-        
-        % Get normalized time series for all but this specific schaefer ROI
-        z_all = schaefer;
-        z_all(:,s) = [];
-        z_all = zscore(table2array(z_all));
 
-        % PCA of the normalized time series. ess contains the PC time
-        % series
-        [~,ess,~,~,expl] = pca(z_all);
-        
-        % Find the set that explains 95% of the variance
-        z_inds = cumsum(expl)<=95;
-        z = ess(:,z_inds);
-        
-        % Compute partial correlation
-        Rp(t,s) = partialcorr(thal{:,t},schaefer{:,s},z);
-        
-    end
+    % PCA of the normalized time series, excluding the specific schaefer
+    % ROI we are working on. ess contains the PC time series.
+    [~,ess,~,~,expl] = pca( ...
+        schaefer{:,[1:s-1 s+1:end]}, ...
+        'Centered',true, ...
+        'VariableWeights','variance' ...
+        );
+    
+    % Find the set that explains 95% of the variance
+    z_inds = cumsum(expl)<=95;
+    z = ess(:,z_inds);
+    
+    % Compute partial correlation
+    Rp(:,s) = partialcorr(thal{:,:},schaefer{:,s},z);
+    
 end
 
 Rp = array2table( ...
