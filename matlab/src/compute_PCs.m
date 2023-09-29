cf014b09af1a35f27ce3647fce1c112f02138e67
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-09-29 10:57:18 -0500

    pc reorg

--- matlab/src/compute_PCs.m
+++ matlab/src/compute_PCs.m
@@ -1,7 +1,10 @@
 function result = compute_PCs(Rthal,densities)
 
-% PC
-PCs = [];
+% Computes participation coef for the thalamus regions in an incomplete
+% thalamus x schaefer (partial) correlation matrix, at a range of densities
+
+result = table();
+ct = 0;
 for d = densities
     thisR = table2array(Rthal.R);
 
@@ -11,10 +14,15 @@ for d = densities
     % Or binarize
     thisR = double(thisR >= quantile(thisR(:),1-d));
     
-    PCs = [PCs; participation_coeff(thisR',Rthal.colinfo.Network)];
+    PCs = participation_coeff(thisR',Rthal.colinfo.Network);
+
+    for r = 1:size(PCs,2)
+        ct = ct + 1;
+        result.density(ct,1) = d;
+        result.Region{ct,1} = Rthal.rowinfo.Region{r};
+        q = strsplit(Rthal.rowinfo.Region{r},'_');
+        result.ROI_Set{ct,1} = q{1};
+        result.roi_PC(ct,1) = PCs(r);
+    end
 end
 
-% Organize and write to file
-result = array2table(densities','VariableNames',{'density'});
-result = [result array2table(PCs,'VariableNames',Rthal.rowinfo.Region)];
-%writetable(result,fullfile(inp.out_dir,['PC_' tag '.csv']));
