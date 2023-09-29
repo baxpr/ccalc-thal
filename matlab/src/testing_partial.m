5539681e0af71341f22500c6584f20680691b1e2
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-09-29 11:13:36 -0500

    lowdens

--- matlab/src/testing_partial.m
+++ matlab/src/testing_partial.m
@@ -4,7 +4,7 @@
 %    schaefer-thalamus partial correlation matrix
 
 out_dir = '../../OUTPUTS';
-densities = 0:0.05:0.95;
+densities = 0:0.005:0.1;
 
 % Get the time series data and ROI info previously created by
 % roi_extract.sh
@@ -21,7 +21,34 @@ Rp_schaefer_thomas = get_partial_matrix_2(schaefer,thomas);
 
 %% 
 % Compute PC at each density threshold for the Schaefer x Thalamus matrices
-PC_yeo = compute_PCs(Rp_schaefer_yeo,densities);
+result = compute_PCs(Rp_schaefer_yeo,densities);
 
-% Compute WMD
 
+%% Summary plot for all ROIs
+all_density = [];
+all_degree = [];
+all_scaledPC = [];
+for r = unique(result.Region)'
+    d = result(strcmp(result.Region,r{1}),:);
+    d = sortrows(d,'density');
+    all_density(end+1,:) = d.density';
+    all_degree(end+1,:) = d.roi_degree';
+    all_scaledPC(end+1,:) = d.roi_scaledPC';
+end
+
+figure(1); clf
+
+for r = 1:size(all_density,1)
+    
+    subplot(1,2,1); hold on
+    plot(all_density(r,:),all_degree(r,:),'-o')
+    xlabel('Density')
+    ylabel('ROI Degree')
+
+    subplot(1,2,2); hold on
+    plot(all_density(r,:),all_scaledPC(r,:),'-o')
+    xlabel('Density')
+    ylabel('ROI scaledPC')
+    
+    
+end
